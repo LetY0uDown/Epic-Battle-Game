b6f33ffdbdb9d850a3e31366d1ee62c3da5e592f
Maks <maks.manin.04@gmail.com>
2022-11-10 13:09:23 +1000

    чё-то наделал

---
+++ Models/Game/BattleAction.cs
@@ -0,0 +1,25 @@
+﻿using Models.Abstract;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.Game;
+
+[Table(nameof(BattleAction))]
+public class BattleAction : Model
+{
+    public int RoomID { get; set; }
+
+    public Character Sender { get; set; }
+
+    public Character? Target { get; set; }
+
+    public Type ActionType { get; set; }
+
+    public enum Type
+    {
+        Attack,
+        Defend,
+        Heal,
+        Swashbuckle,
+        Nuke
+    }
+}
